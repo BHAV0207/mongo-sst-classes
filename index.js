dc732aecadfa29cca179ff614db2138cacf2f357
bhavya jain <bhavyajain4274@gmail.com>
2024-07-08 12:08:51 +0530

    mongo

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+
+app.use(express.json());
+
+const mongoose = require('mongoose');
+const { Route } = require('react-router-dom');
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>{
+    console.log("Db connected");
+})
+.catch((err) =>{
+    console.log("Failed");
+    console.log(err);
+});
+
+app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
+
+
+app.listen(8086, () =>{
+    console.log("Server Started at 8086")
+})
